6afcb6b0c101ec23ad6bbbaa1dd700837bd5130a
Mxaka <sinoxolo.mxaka@gmail.com>
2026-08-16 12:53:58 +0200

    initial commit

---
+++ src/main/java/com/sinoxolo/taskmanagementAPI/TaskmanagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.sinoxolo.taskmanagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementApiApplication.class, args);
+	}
+
+}
