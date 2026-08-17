44ca952093f88964f8809d2e0de7b18b84f447cd
Mxaka <sinoxolo.mxaka@gmail.com>
2026-08-17 20:10:45 +0200

    added user information: fields that identify a user

--- src/main/java/com/sinoxolo/taskmanagementAPI/domain/user/User.java
+++ src/main/java/com/sinoxolo/taskmanagementAPI/domain/user/User.java
@@ -1,4 +1,49 @@
 package com.sinoxolo.taskmanagementAPI.domain.user;
 
+import java.util.Date;
+
 public class User {
+    private UserId id;
+    private String userName;
+    private String password;
+    private Role role;
+
+    public User(UserId id, String userName, String password, Role role) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UserId getId() {
+        return id;
+    }
+
+    public void setId(UserId id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
