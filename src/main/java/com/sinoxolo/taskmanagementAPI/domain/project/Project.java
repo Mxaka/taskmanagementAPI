068728ea440d83a9047ea2b3882ca301ddbdd752
Mxaka <sinoxolo.mxaka@gmail.com>
2026-08-17 20:32:56 +0200

    Initial implementation of Project and ProjectRepository

--- src/main/java/com/sinoxolo/taskmanagementAPI/domain/project/Project.java
+++ src/main/java/com/sinoxolo/taskmanagementAPI/domain/project/Project.java
@@ -1,4 +1,61 @@
 package com.sinoxolo.taskmanagementAPI.domain.project;
 
+import com.sinoxolo.taskmanagementAPI.domain.user.UserId;
+
+import java.util.Date;
+
 public class Project {
+    private ProjectId id;
+    private String name;
+    private String description;
+    private UserId leadId;
+    private Date createdAt;
+
+    public Project(ProjectId id, String name, String description, UserId leadId, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.leadId = leadId;
+        this.createdAt = createdAt;
+    }
+
+    public ProjectId getId() {
+        return id;
+    }
+
+    public void setId(ProjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UserId getLeadId() {
+        return leadId;
+    }
+
+    public void setLeadId(UserId leadId) {
+        this.leadId = leadId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/sinoxolo/taskmanagementAPI/domain/project/ProjectRepository.java
+++ src/main/java/com/sinoxolo/taskmanagementAPI/domain/project/ProjectRepository.java
@@ -1,4 +1,13 @@
 package com.sinoxolo.taskmanagementAPI.domain.project;
 
-public class ProjectRepository {
+import com.sinoxolo.taskmanagementAPI.domain.user.User;
+import com.sinoxolo.taskmanagementAPI.domain.user.UserId;
+
+public interface ProjectRepository {
+    Project findProjectById(ProjectId id);
+    Project findProjectByName(String name);
+    Project findProjectByDescription(String description);
+    User findProjectLeadById(UserId leadId);
+    Project updateProjectDescription(ProjectId id);
+    void deleteProject(ProjectId id);
 }
